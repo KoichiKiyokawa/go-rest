41de86dd4bbf5a509be62b89e9cf6e2923567f88
K.Kiyokawa <koichi20110068@gmail.com>
2020-03-13 22:38:07 +0900

    refactor: main.go

--- main.go
+++ main.go
@@ -1,38 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Todo struct {
-  ID    uint    `json:"id"`
+	ID    uint   `json:"id"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
-  Done bool `json:"done"`
+	Done  bool   `json:"done"`
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func connect() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "./db.sqlite3")
 	if err != nil {
 		log.Fatal(err)
 	}
-  db.LogMode(true)
-	defer db.Close()
+	db.LogMode(true)
 
 	db.AutoMigrate(&Todo{})
+	return db
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	db := connect()
+  defer db.Close()
 
 	var todos []Todo
 	db.Find(&todos)
-	fmt.Printf("%v\n", todos)
 
 	w.Header().Set("Content-Type", "application/json")
+  resp, _ := json.Marshal(todos)
+  w.Write(resp)
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	db := connect()
+  defer db.Close()
+
+	title := r.URL.Query().Get("title")
+	body := r.URL.Query().Get("body")
+	newTodo := Todo{Title: title, Body: body, Done: false}
+  db.NewRecord(newTodo)
+	db.Create(&newTodo)
 }
 
 func main() {
+  http.HandleFunc("/new",newHandler)
 	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
